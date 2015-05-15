ae95380b3314d7b0b277feba1d74dd1118413836
Alex <alexpetit12@hotmail.com>
2015-05-15 13:04:05 -0400

    Codeforces C++

---
+++ 451A-GameWithSticks.cpp
@@ -0,0 +1,19 @@
+//Codeforces
+//451A - Game with sticks
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	int m, n;
+
+	cin >> m >> n;
+
+	if(min(m, n) % 2 == 0)
+		cout << ("Malvika");
+	else
+		cout << ("Akshat");
+
+	return 0;
+}
